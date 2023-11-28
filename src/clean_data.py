e75ac2468af0e50b420bf44433b349d4f43df3b8
Nayotama Pradipta <n4y0t4m4@gmail.com>
2023-11-28 21:59:59 +0700

    feat: delete emojis

--- src/clean_data.py
+++ src/clean_data.py
@@ -10,11 +10,6 @@ import re
 # Handle misspelling ? 
     # mrica, lada bitam, bawah putih, bawah merah, dst 
 # Tambah words_to_delete kalo ada kata yang belum dimasukkin 
-# Handle symbol-symbol seperti astrix (*), more or less (-+), emoji, dll
-
-
-dataset = input("Dataset: ")
-input_file = f'./data/raw/dataset-{dataset}.csv'
 
 # Words & Regex
 words_to_delete = ["gram", "kilo", "kg", "liter", "ons", "ml",
@@ -22,8 +17,8 @@ words_to_delete = ["gram", "kilo", "kg", "liter", "ons", "ml",
                    "sedikit", "secukupnya", "sckpnya", "secukup nya", "se cukupnya", "secukup",
                    "sdm", "sdt", "sct", "siung", "batang", "btg", "lembar", "lbr", "lb", "cup",
                    "segenggam", "genggam", "sejumput", "se jumput", "jumput", "bj",
-                   "sendok makan", "sendok teh", "sendok", "seikat", "ikat",
-                   "seruas jari", "ruas jari", "ruas", "bungkus", "bks", 
+                   "sendok makan", "sendok teh", "sendok", "seikat", "ikat", "sck",
+                   "seruas jari", "ruas jari", "seruas", "ruas", "bungkus", "bks", 
                    "sepiring", "piring", "biji", "sesuai selera", "tangkai"]
                     # Manual: ekor
 standalone_words = ['g', "gr"]
@@ -34,8 +29,13 @@ def clean_regex(word):
     cleaned_word = re.sub(r'\b(?:\d+/\d+|\d+)\b', '', cleaned_word)
     # Delete anything with parentheses
     cleaned_word = re.sub(r'\([^)]*\)', '', cleaned_word)
-    
-    
+    # Delete emojis: 
+    cleaned_word = re.sub(r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])', '', cleaned_word)
+    # Delete more or less 
+    cleaned_word = re.sub(r'\+\-|\-\+', '', cleaned_word)
+    # Delete astrix
+    cleaned_word = cleaned_word.replace('*', '')
+
     for delete_word in words_to_delete:
         cleaned_word = cleaned_word.replace(delete_word, '')
     
@@ -48,22 +48,32 @@ def clean_regex(word):
     cleaned_word = re.sub(r'\s*--\s*', '--', cleaned_word)
     return cleaned_word.strip()
 
+def update_ingredients(rows, input_file, ingredients_only):
+    with open(input_file, 'r', encoding="utf8") as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        rows.append(header)
+        for row in csvreader: 
+            # Delete unnecessary words on ingredients column
+            cleaned_ingredients = clean_regex(row[1].lower())
+            row[1] = cleaned_ingredients
+            if (ingredients_only):
+                rows.append(cleaned_ingredients)
+            else: 
+                rows.append(row)
+    return rows 
 
-
-
-rows = []
-with open(input_file, 'r', encoding="utf8") as file:
-    csvreader = csv.reader(file)
-    header = next(csvreader)
-    rows.append(header)
-    for row in csvreader: 
-        # Delete unnecessary words on ingredients column
-        cleaned_ingredients = clean_regex(row[1].lower())
-        row[1] = cleaned_ingredients
-        rows.append(row)
-
-output_file = f'./data/cleaned/dataset-{dataset}-cleaned.csv'
-
-with open(output_file, 'w', newline='', encoding='utf8') as file: 
-    csvwriter = csv.writer(file)
-    csvwriter.writerows(rows)
+if __name__ == "__main__": 
+    dataset = input("Dataset: ")
+    ingredients_only = False # Set True for testing (CSV will only contain ingredient column)
+    input_file = f'./data/raw/dataset-{dataset}.csv'
+    rows = []
+    rows = update_ingredients(rows, input_file, ingredients_only)
+    output_file = f'./data/cleaned/dataset-{dataset}-cleaned.csv'
+    with open(output_file, 'w', newline='', encoding='utf8') as file: 
+        csvwriter = csv.writer(file)
+        if (ingredients_only):
+            for row in rows: 
+                csvwriter.writerow([row])
+        else: 
+            csvwriter.writerows(rows)
