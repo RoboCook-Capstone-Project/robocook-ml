c7f4f17ec46b98548be293da21e97606f353cdfe
rikurakha <rakhasennin@gmail.com>
2023-12-18 15:07:51 +0700

    fix: update the input for fusion_recipes

--- src/fusion.py
+++ src/fusion.py
@@ -11,19 +11,16 @@ vectorizer = CountVectorizer(tokenizer=lambda x: x.split('--'), token_pattern=No
 bahan_matrix = vectorizer.fit_transform(data['Ingredients'])
 
 # Fungsi untuk Dipanggil
-# INPUT: List(strings) bahan1, bahan2
-def fusion_recipes(id_1, id_2, top_n=5):
-    # Gabung bahan dari resep input
-    resep_input = [id_1, id_2]
-    
-    # UBAH JADI AMBIL INGREDIENTS DARI DATABASE
-    #              BAGIAN YANG INI
-    resep_gabung = data.loc[resep_input, 'Ingredients'].str.cat(sep='--')
-    
-    resep_gabung_vector = vectorizer.transform([resep_gabung])
+# INPUT: (string) bahan1, bahan2
+# OUTPUT: list of ID
+# Contoh input: 'chicken breast--crushed tomatoes--diced oninon' dan 'ground beef--red pepper'
+def find_closest_recipes(bahan1, bahan2, top_n=5):
+    # Gabung bahan resep input
+    bahan_gabung = f"{bahan1}--{bahan2}"
+    bahan_gabung_vector = vectorizer.transform([bahan_gabung])
 
-    # Cari top 5 resep yang paling mirip
-    resep_fusion = cosine_similarity(resep_gabung_vector, bahan_matrix).flatten()
-    resep_fusion = resep_fusion.argsort()[:-top_n-1:-1]
+    # Calculate cosine similarity between the combined vector and all recipes
+    resep_fusion = cosine_similarity(bahan_gabung_vector, bahan_matrix).flatten()
+    resep_ouput = resep_fusion.argsort()[:-top_n-1:-1]
 
-    return data.loc[resep_fusion, 'id'].tolist()
+    return data.loc[resep_ouput, 'id'].tolist()
